8689c49f56ed95adfbfcd32e6633a40c8cb61a21
nurike05 nurike05 <nurike1205@gmail.com>
2020-12-15 03:15:42 +0600

    Первая работа

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Greeting greeting = new Greeting();
+            Applausing applausing = new Applausing();
+            greeting.SayHello();
+            applausing.Applause();
+            Console.ReadLine();
+        }
+    }
+}
